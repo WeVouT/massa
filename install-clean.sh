9d6f79036fd48b342a99692a432765d32e7f55d2
Christoph Zeltner <christoph.zeltner@softaware.at>
2021-11-16 16:39:53 +0100

    Update install-clean.sh

--- install-clean.sh
+++ install-clean.sh
@@ -1,7 +1,8 @@
 apt-get update
-sudo apt --assume-yes install pkg-config curl git build-essential libssl-dev
+apt-get --assume-yes install pkg-config curl git build-essential libssl-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 rustup toolchain install nightly
 rustup default nightly
 git clone --branch testnet https://gitlab.com/massalabs/massa.git
+
