8abdf63f8d332eb20de653014104f6cb84a13c34
Christoph Zeltner <christoph.zeltner@softaware.at>
2021-11-16 17:13:54 +0100

    update structure

--- install.sh
+++ install.sh
@@ -1,6 +1,32 @@
 apt-get update
+apt-get -y install pkg-config
+apt-get -y install curl
+apt-get -y install git 
+apt-get -y install build-essential 
+apt-get -y install libssl-dev
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+rustup toolchain install nightly
+rustup default nightly
+git clone --branch testnet https://gitlab.com/massalabs/massa.git
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+echo # RUSTING
+cd /root/massa/massa-client/
+cargo build --release
+echo # RUSTING
+cd /root/massa/massa-node/
+RUST_BACKTRACE=full cargo build --release 
 
-docker build /root
+echo ############################################################
+echo ############################################################
+echo ########               MASSA  SETUP                  #######
+echo ############################################################
+echo ############################################################
+cd /etc/systemd/system
+wget https://raw.githubusercontent.com/wevout/massa/main/massa.service
+
+systemctl daemon-reload
+systemctl start massa.service
+
+cd /root/massa/massa-client/
+cargo run --release
